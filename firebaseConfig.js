e230b462d25cc021796a907e9325d3e668e36ff2
r-agarwal24 <r.agarwal@aionmanagement.com>
2024-12-12 14:29:46 -0500

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAt_UgVnzRq-E-9MrUj02Cyupy_mL1Gfa0",
+  authDomain: "status-update-c5a72.firebaseapp.com",
+  databaseURL: "https://status-update-c5a72-default-rtdb.firebaseio.com",
+  projectId: "status-update-c5a72",
+  storageBucket: "status-update-c5a72.firebasestorage.app",
+  messagingSenderId: "706934655596",
+  appId: "1:706934655596:web:25c622f2d93e6648e72649",
+  measurementId: "G-FF3BKTGF69"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(firebase.app().name);
+const database = firebase.database();
